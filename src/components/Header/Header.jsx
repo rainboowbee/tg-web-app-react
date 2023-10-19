7c0435913a77f05c32a40ed1757a4c0e848acc6c
rainboowbee <rainboowbee@gmail.com>
2023-10-19 18:03:34 +0300

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from "../Button/Button";
 import './Header.css';
 import {useTelegram} from "../../hooks/useTelegram";
@@ -6,16 +6,23 @@ import FormButton from "../FormButton/FormButton";
 
 const Header = () => {
     const {user, onClose} = useTelegram();
-    
-
+    const [isOpen, setOpen] = useState();
     return (
-        <div className={'header'}>
-            <Button onClick={onClose}>Закрыть</Button>
-            <FormButton>Заполнить форму</FormButton>
-            <span className={'username'}>
-                {user?.username}
-            </span>
-        </div>
+        <header className="header">
+            <span className="header__name">libcom</span>
+            <nav className={`header__nav ${isOpen ? "active" : ""}`}>
+                <ul className="header__nav-list">
+                    <li className="header__nav-item">Main</li>
+                    <li className="header__nav-item">About</li>
+                    <li className="header__nav-item">Adress</li>
+                    <li className="header__nav-item">Contact</li>
+                    <li className="header__nav-item">Login</li>
+                </ul>
+            </nav>
+            <button className="header__menu-button" onClick={()=> setOpen(!isOpen)}>
+                Меню
+            </button>
+        </header>
     );
 };
 
